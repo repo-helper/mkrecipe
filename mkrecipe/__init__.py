d6db8dc791e12ca65ca8f1b0ae32c27fefd25731
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-29 22:35:06 +0100

    Bump version v0.1.1 -> v0.1.2

--- mkrecipe/__init__.py
+++ mkrecipe/__init__.py
@@ -47,7 +47,7 @@ from mkrecipe.config import load_toml
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.1"
+__version__: str = "0.1.2"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["MaryBerry", "make_recipe"]
